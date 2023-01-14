d415ca87d62ea9a0e64a66cc714c74668e2e7815
bigheha <chispinsky1337@gmail.com>
2023-01-14 18:11:23 +0200

    implemented createOption function, renamed setAttributes to setFieldAttributes

--- src/forms.js
+++ src/forms.js
@@ -1,7 +1,7 @@
 const newProjectForm = document.createElement('form');
-const projectTitleField = document.createElement('input');
-setAttributes(projectTitleField, 'text', 'projectTitle');
 
+const projectTitleField = document.createElement('input');
+setFieldAttributes(projectTitleField, 'text', 'projectTitle');
 const addProjectBtn = document.createElement('input');
 addProjectBtn.setAttribute('type', 'input');
 
@@ -10,33 +10,21 @@ newProjectForm.append(projectTitleField, addProjectBtn);
 const newTodoForm = document.createElement('form');
 
 const todoTitleField = document.createElement('input');
-setAttributes(todoTitleField, 'text', 'todoTitle');
+setFieldAttributes(todoTitleField, 'text', 'todoTitle');
 const todoDescriptionField = document.createElement('input');
-setAttributes(todoDescriptionField, 'text', 'todoDescription');
+setFieldAttributes(todoDescriptionField, 'text', 'todoDescription');
 const todoDueDateField = document.createElement('input');
-setAttributes(todoDueDateField, 'date', 'todoDueDate');
+setFieldAttributes(todoDueDateField, 'date', 'todoDueDate');
 
 const todoPriorityField = document.createElement('select');
 todoPriorityField.setAttribute('id', 'todoPriority');
-
-const lowPriority = document.createElement('option');
-lowPriority.setAttribute('value', 'low priority');
-lowPriority.innerHTML = 'low priority';
-todoPriorityField.appendChild(lowPriority);
-
-const regularPriority = document.createElement('option');
-regularPriority.setAttribute('value', 'regular priority');
-regularPriority.innerHTML = 'regular priority';
-todoPriorityField.appendChild(regularPriority);
-
-const highPriority = document.createElement('option');
-highPriority.setAttribute('value', 'high priority');
-highPriority.innerHTML = 'high priority'
-todoPriorityField.appendChild(highPriority);
+const lowPriority = createOption('low priority');
+const regularPriority = createOption('regular priority');
+const highPriority = createOption('high priority');
+todoPriorityField.append(lowPriority, regularPriority, highPriority);
 
 const addTodoBtn = document.createElement('input');
 addTodoBtn.setAttribute('type', 'submit');
-
 newTodoForm.append(
     todoTitleField, todoDescriptionField, 
     todoDueDateField, todoPriorityField, addTodoBtn
@@ -44,7 +32,13 @@ newTodoForm.append(
 
 export {newProjectForm, newTodoForm};
 
-function setAttributes (element, type, idAndName) {
+function createOption(valueAndText) {
+    const option = document.createElement('option');
+    option.setAttribute('value', valueAndText);
+    option.innerHTML = valueAndText;
+    return option;
+}
+function setFieldAttributes (element, type, idAndName) {
     element.setAttribute('type', type);
     element.setAttribute('id', idAndName);
     element.setAttribute('name', idAndName);
